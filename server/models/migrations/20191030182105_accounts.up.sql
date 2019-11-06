1dcf8d99ddd7dd2805b4fc59f9039cc144220c0a
johannes Heimbach <heimbachjohannes@gmail.com>
2019-11-06 19:39:01 +0100

    add nfc_chip_uid field to accounts

--- server/models/migrations/20191030182105_accounts.up.sql
+++ server/models/migrations/20191030182105_accounts.up.sql
@@ -1,10 +1,14 @@
 CREATE TABLE `accounts`
 (
-        `id` integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
-        `name` varchar(255) NOT NULL,
-        `description` TEXT,
-        `saldo` FLOAT NOT NULL DEFAULT 0,
-        `group_id` INTEGER
+    `id`          integer PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name`        varchar(255)        NOT NULL,
+    `description` TEXT,
+    `saldo`       FLOAT               NOT NULL DEFAULT 0,
+    `group_id`    INTEGER,
+    # according to ISO 14443-3A for nfc tags, uids are 4-10 bytes long (hex 2 chars per byte are 20 max).
+    # source: https://www.nxp.com/docs/en/application-note/AN10927.pdf
+    `nfc_chip_uid`     char(20) UNIQUE  NOT NULL
 );
 
-ALTER TABLE `accounts` ADD FOREIGN KEY (`group_id`) REFERENCES `account_groups`(`id`);
+ALTER TABLE `accounts`
+    ADD FOREIGN KEY (`group_id`) REFERENCES `account_groups` (`id`);
