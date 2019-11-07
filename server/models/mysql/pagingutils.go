bd47170ed43fbf4015f0551cbdd5ce400b8b7f07
johannes Heimbach <heimbachjohannes@gmail.com>
2019-11-07 13:37:08 +0100

    refactoring

--- server/models/mysql/pagingutils.go
+++ server/models/mysql/pagingutils.go
@@ -2,15 +2,19 @@ package mysql
 
 import "database/sql"
 
+// pageOffset returns offset from 0 based on page and size.
+// if page is 0 or negative, pageOffset will return 0
 func pageOffset(page int, size int) int {
 	if page <= 0 {
-		page = 1
+		return 0
 	}
 
 	offset := (page - 1) * size
 	return offset
 }
 
+// countAllIds returns int for given query.
+// query should have style of SELECT COUNT(...) FROM ...
 func countAllIds(db *sql.DB, query string, args ...interface{}) (int, error) {
 	var count int
 	err := db.QueryRow(query, args...).Scan(&count)
@@ -21,6 +25,8 @@ func countAllIds(db *sql.DB, query string, args ...interface{}) (int, error) {
 	return count, nil
 }
 
+// maxPageCount returns max number of pages that will contain all elements
+// based on element count and size of page
 func maxPageCount(count, size int) int {
 	return (count + size - 1) / size
 }
