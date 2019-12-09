c17da01167a161e5711119da8e37bd3d6216900f
johannes Heimbach <heimbachjohannes@gmail.com>
2019-12-09 18:49:34 +0100

    add groupserver

---
+++ server/groups.go
@@ -0,0 +1,66 @@
+package server
+
+import (
+	"context"
+	"errors"
+
+	"github.com/JHeimbach/nfc-cash-system/server/api"
+	"github.com/JHeimbach/nfc-cash-system/server/models"
+)
+
+var (
+	ErrCouldNotCreateGroup = errors.New("could not create group")
+)
+
+type groupserver struct {
+	storage models.GroupStorager
+}
+
+func (g *groupserver) List(ctx context.Context, req *api.GroupListRequest) (*api.Groups, error) {
+	groups, err := g.storage.GetAll()
+	if err != nil {
+		return nil, ErrGetAll
+	}
+	return groups, nil
+}
+
+func (g *groupserver) Create(ctx context.Context, req *api.Group) (*api.Group, error) {
+	group, err := g.storage.Create(req.Name, req.Description, req.CanOverdraw)
+
+	if err != nil {
+		return nil, ErrCouldNotCreateGroup
+	}
+
+	return group, nil
+}
+
+func (g *groupserver) Get(ctx context.Context, req *api.IdRequest) (*api.Group, error) {
+	group, err := g.storage.Read(req.Id)
+
+	if err != nil {
+		return nil, ErrNotFound
+	}
+
+	return group, nil
+}
+
+func (g *groupserver) Update(ctx context.Context, req *api.Group) (*api.Group, error) {
+	group, err := g.storage.Update(*req)
+	if err != nil {
+		return nil, ErrSomethingWentWrong
+	}
+	return group, nil
+}
+
+func (g *groupserver) Delete(ctx context.Context, req *api.IdRequest) (*api.Status, error) {
+	err := g.storage.Delete(req.Id)
+
+	if err != nil {
+		return &api.Status{
+			Success:      false,
+			ErrorMessage: ErrSomethingWentWrong.Error(),
+		}, ErrSomethingWentWrong
+	}
+
+	return &api.Status{Success: true}, nil
+}
---
+++ server/groups_test.go
@@ -0,0 +1,163 @@
+package server
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/JHeimbach/nfc-cash-system/server/api"
+)
+
+type groupstorage struct {
+	create func(name, description string, canOverdraw bool) (*api.Group, error)
+	getAll func() (*api.Groups, error)
+	read   func(id int32) (*api.Group, error)
+	update func(group *api.Group) (*api.Group, error)
+	delete func(id int32) error
+}
+
+func (g *groupstorage) Create(name, description string, canOverdraw bool) (*api.Group, error) {
+	return g.create(name, description, canOverdraw)
+}
+
+func (g *groupstorage) GetAll() (*api.Groups, error) {
+	return g.getAll()
+}
+
+func (g *groupstorage) Read(id int32) (*api.Group, error) {
+	return g.read(id)
+}
+
+func (g *groupstorage) Update(group api.Group) (*api.Group, error) {
+	return g.update(group)
+}
+
+func (g *groupstorage) Delete(id int32) error {
+	return g.delete(id)
+}
+
+func TestGroupserver_List(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   *api.GroupListRequest
+		want    *api.Groups
+		wantErr error
+	}{
+		{
+			name:  "get simple list of accounts",
+			input: &api.GroupListRequest{},
+			want: &api.Groups{
+				Groups: genGroupModels(2),
+			},
+		},
+		{
+			name:    "has error",
+			input:   &api.GroupListRequest{},
+			wantErr: ErrGetAll,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			a := &groupserver{
+				storage: &groupstorage{getAll: func() (*api.Groups, error) {
+					if tt.wantErr != nil {
+						return nil, sql.ErrNoRows
+					}
+					return tt.want, nil
+				},
+				},
+			}
+			got, err := a.List(context.Background(), tt.input)
+
+			if err != tt.wantErr {
+				t.Errorf("List() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("List() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestGroupserver_Create(t *testing.T) {
+	tests := []struct {
+		name    string
+		input   *api.Group
+		want    *api.Group
+		wantErr error
+	}{
+		{
+			name: "create group",
+			input: &api.Group{
+				Name:        "test group",
+				Description: "test",
+				CanOverdraw: false,
+			},
+			want: &api.Group{
+				Id:          1,
+				Name:        "test group",
+				Description: "test",
+				CanOverdraw: false,
+			},
+		},
+		{
+			name: "create group",
+			input: &api.Group{
+				Name:        "test group",
+				Description: "test",
+				CanOverdraw: false,
+			},
+			wantErr: ErrCouldNotCreateGroup,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			server := groupserver{
+				storage: &groupstorage{
+					create: func(name, description string, canOverdraw bool) (*api.Group, error) {
+						if tt.wantErr != nil {
+							return nil, tt.wantErr
+						}
+						ret := tt.want
+						ret.Id = 1
+						return ret, nil
+					},
+				},
+			}
+
+			got, err := server.Create(context.Background(), tt.input)
+			if tt.wantErr != nil {
+				if err != tt.wantErr {
+					t.Errorf("got err %v, expected %v", err, tt.wantErr)
+				}
+				return
+			}
+			if err != nil {
+				t.Fatalf("did not expect error, got %v", err)
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("got %v, expected %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func genGroupModels(num int) []*api.Group {
+	var groups []*api.Group
+	for i := 0; i < num; i++ {
+		groups = append(groups, &api.Group{
+			Id:          int32(i + 1),
+			Name:        fmt.Sprintf("group name %d", i+1),
+			Description: "description",
+			CanOverdraw: i%2 == 0,
+		})
+	}
+
+	return groups
+}
