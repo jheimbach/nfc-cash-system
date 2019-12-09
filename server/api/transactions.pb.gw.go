c043a78b4d8b1fb11420e5e20deb587c7b9ded22
johannes Heimbach <heimbachjohannes@gmail.com>
2019-12-09 20:04:25 +0100

    add transaction all api method

--- server/api/transactions.pb.gw.go
+++ server/api/transactions.pb.gw.go
@@ -32,6 +32,39 @@ var _ = utilities.NewDoubleArray
 var _ = descriptor.ForMessage
 
 var (
+	filter_TransactionsService_All_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+)
+
+func request_TransactionsService_All_0(ctx context.Context, marshaler runtime.Marshaler, client TransactionsServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq TransactionAllRequest
+	var metadata runtime.ServerMetadata
+
+	if err := req.ParseForm(); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+	if err := runtime.PopulateQueryParameters(&protoReq, req.Form, filter_TransactionsService_All_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.All(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_TransactionsService_All_0(ctx context.Context, marshaler runtime.Marshaler, server TransactionsServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq TransactionAllRequest
+	var metadata runtime.ServerMetadata
+
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_TransactionsService_All_0); err != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.All(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
+var (
 	filter_TransactionsService_List_0 = &utilities.DoubleArray{Encoding: map[string]int{"account_id": 0}, Base: []int{1, 1, 0}, Check: []int{0, 1, 2}}
 )
 
@@ -251,6 +284,26 @@ func local_request_TransactionsService_Get_0(ctx context.Context, marshaler runt
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
 func RegisterTransactionsServiceHandlerServer(ctx context.Context, mux *runtime.ServeMux, server TransactionsServiceServer) error {
 
+	mux.Handle("GET", pattern_TransactionsService_All_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateIncomingContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_TransactionsService_All_0(rctx, inboundMarshaler, server, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TransactionsService_All_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_TransactionsService_List_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -352,6 +405,26 @@ func RegisterTransactionsServiceHandler(ctx context.Context, mux *runtime.ServeM
 // "TransactionsServiceClient" to call the correct interceptors.
 func RegisterTransactionsServiceHandlerClient(ctx context.Context, mux *runtime.ServeMux, client TransactionsServiceClient) error {
 
+	mux.Handle("GET", pattern_TransactionsService_All_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_TransactionsService_All_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_TransactionsService_All_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	mux.Handle("GET", pattern_TransactionsService_List_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
@@ -416,6 +489,8 @@ func RegisterTransactionsServiceHandlerClient(ctx context.Context, mux *runtime.
 }
 
 var (
+	pattern_TransactionsService_All_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"v1", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
+
 	pattern_TransactionsService_List_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "account", "account_id", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
 
 	pattern_TransactionsService_Create_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 1, 0, 4, 1, 5, 2, 2, 3}, []string{"v1", "account", "account.id", "transactions"}, "", runtime.AssumeColonVerbOpt(true)))
@@ -424,6 +499,8 @@ var (
 )
 
 var (
+	forward_TransactionsService_All_0 = runtime.ForwardResponseMessage
+
 	forward_TransactionsService_List_0 = runtime.ForwardResponseMessage
 
 	forward_TransactionsService_Create_0 = runtime.ForwardResponseMessage
