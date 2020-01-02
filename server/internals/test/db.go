a130abc81f07694b30817e92def66135fff8383d
johannes Heimbach <heimbachjohannes@gmail.com>
2020-01-02 15:37:06 +0100

    extract test db from mysql in internal test package

---
+++ server/internals/test/db.go
@@ -0,0 +1,79 @@
+package test
+
+import (
+	"database/sql"
+	"io/ioutil"
+	"testing"
+
+	"github.com/JHeimbach/nfc-cash-system/server/internals/database"
+)
+
+// testDb will hold a db connection for multiple usages
+type testDb struct {
+	db             *sql.DB
+	teardownScript string
+}
+
+// initDb connects to the database. will skip the calling test if TEST_DB_DSN is not set
+// or connection to database is unsuccessful
+func (tdb testDb) initDb(t *testing.T) *sql.DB {
+	if tdb.db != nil {
+		return tdb.db
+	}
+
+	db, err := database.OpenDatabase("${TEST_DB_NAME}:${TEST_DB_NAME}@tcp(${TEST_DB_NAME}-test.db)/${TEST_DB_NAME}?parseTime=true&multiStatements=true")
+	if err != nil {
+		t.Skipf("could not connect to database, skipping test, err: %v", err)
+	}
+	// db.SetConnMaxLifetime(1 * time.Second)
+	// db.SetMaxOpenConns(100)
+	return db
+}
+
+var testDBconn testDb
+
+// getDb connects to database and returns db (*sql.DB) a setup function and a teardown function
+// setup calls migration up scripts to bring database to latest and can
+// receive filenames (strings) for additional setup scripts (sql-files)
+// teardown calls migration down scripts
+//
+// setup and teardown can be called multiple times per test
+func GetDb(t *testing.T, dbName, teardownScriptFile string, migrationDir string) (db *sql.DB, setup func(...string), teardown func()) {
+	t.Helper()
+	db = testDBconn.initDb(t)
+
+	if err := db.Ping(); err != nil {
+		t.Skipf("could not connect to database, skipping test, err: %v", err)
+	}
+
+	testDBconn.teardownScript = func() string {
+		content, err := ioutil.ReadFile(teardownScriptFile)
+		if err != nil {
+			t.Skipf("could not load teardown script: %v", err)
+		}
+		return string(content)
+	}()
+
+	teardown = func() {
+		_, err := db.Exec(testDBconn.teardownScript)
+		if err != nil {
+			t.Skipf("got error from teardown: %v", err)
+		}
+	}
+
+	setup = func(setupScriptFileNames ...string) {
+		if err := database.UpdateDatabase(db, dbName, migrationDir, true); err != nil {
+			t.Skipf("could not migrate database, err: %v", err)
+		}
+		for _, filename := range setupScriptFileNames {
+			setupScript, _ := ioutil.ReadFile(filename)
+			_, err := db.Exec(string(setupScript))
+			if err != nil {
+				teardown()
+				t.Skipf("got error initializing script %q: %v", filename, err)
+			}
+		}
+	}
+
+	return db, setup, teardown
+}
--- server/models/mysql/integration_test.go
+++ server/models/mysql/integration_test.go
@@ -2,108 +2,24 @@ package mysql
 
 import (
 	"database/sql"
-	"io/ioutil"
-	"os"
+	"path"
+	"strings"
 	"testing"
-	"time"
 
-	"github.com/JHeimbach/nfc-cash-system/internals/database"
+	"github.com/JHeimbach/nfc-cash-system/server/internals/test"
 )
 
 const (
-	defaultDbName       = "nfc-cash-system_test"
-	defaultMigrationDir = "../migrations"
+	DbName       = "nfc-cash-system_test"
+	MigrationDir = "../../migrations"
 )
 
-// isIntegrationTest will check if envvar RUN_INTEGRATION is non zero.
-// if RUN_INTEGRATION is zero (or not set), tests that call isIntegrationTest will be skipped
-func isIntegrationTest(t *testing.T) {
-	if getEnvWithDefault("RUN_INTEGRATION", "0") == "0" {
-		t.Skipf("skipping integration tests")
-	}
+func dataFor(t string) string {
+	return path.Join("../../testdata", strings.Join([]string{t, "sql"}, "."))
 }
 
-// genEnvWithDefault is a helper function to get envvar or default value
-func getEnvWithDefault(envName, defaultVal string) string {
-	if val, ok := os.LookupEnv(envName); ok {
-		return os.ExpandEnv(val)
-	}
-	return defaultVal
-}
-
-// testDb will hold a db connection for multiple usages
-type testDb struct {
-	db             *sql.DB
-	teardownScript string
-}
-
-// initDb connects to the database. will skip the calling test if TEST_DB_DSN is not set
-// or connection to database is unsuccessful
-func (tdb testDb) initDb(t *testing.T) *sql.DB {
-	if tdb.db != nil {
-		return tdb.db
-	}
-
-	dsn := getEnvWithDefault("TEST_DB_DSN", "")
-	if dsn == "" {
-		t.Skipf("no database dsn found, skipping test")
-	}
-
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		t.Skipf("could not connect to database, skipping test, err: %v", err)
-	}
-	return db
-}
-
-var testDBconn testDb
-
-// getTestDb connects to database and returns db (*sql.DB) a setup function and a teardown function
-// setup calls migration up scripts to bring database to latest and can
-// receive filenames (strings) for additional setup scripts (sql-files)
-// teardown calls migration down scripts
-//
-// setup and teardown can be called multiple times per test
-func getTestDb(t *testing.T) (db *sql.DB, setup func(...string), teardown func()) {
-	t.Helper()
-	db = testDBconn.initDb(t)
-	db.SetConnMaxLifetime(1 * time.Second)
-	db.SetMaxOpenConns(100)
-
-	if err := db.Ping(); err != nil {
-		t.Skipf("could not connect to database, skipping test, err: %v", err)
-	}
-
-	migrationDir := getEnvWithDefault("DB_MIGRATIONS_DIR", defaultMigrationDir)
-	dbName := getEnvWithDefault("TEST_DB_NAME", defaultDbName)
-	testDBconn.teardownScript = func() string {
-		content, err := ioutil.ReadFile("../testdata/teardown.sql")
-		if err != nil {
-			t.Fatalf("could not load teardown script: %v", err)
-		}
-		return string(content)
-	}()
-
-	teardown = func() {
-		_, err := db.Exec(testDBconn.teardownScript)
-		if err != nil {
-			t.Errorf("got error from teardown: %v", err)
-		}
-	}
-
-	setup = func(setupScriptFileNames ...string) {
-		if err := database.UpdateDatabase(db, dbName, migrationDir, false); err != nil {
-			t.Skipf("could not migrate database, err: %v", err)
-		}
-		for _, filename := range setupScriptFileNames {
-			setupScript, _ := ioutil.ReadFile(filename)
-			_, err := db.Exec(string(setupScript))
-			if err != nil {
-				teardown()
-				t.Fatalf("got error initializing script %q: %v", filename, err)
-			}
-		}
-	}
-
-	return db, setup, teardown
+func getTestDb(t *testing.T) (*sql.DB, func(...string), func()) {
+	migrationDir := test.EnvWithDefault("DB_MIGRATIONS_DIR", MigrationDir)
+	dbName := test.EnvWithDefault("TEST_DB_NAME", DbName)
+	return test.GetDb(t, dbName, dataFor("teardown"), migrationDir)
 }
