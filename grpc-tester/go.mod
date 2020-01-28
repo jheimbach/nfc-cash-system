827289bbf166cecbeb35ef8daac36877ed768209
johannes Heimbach <heimbachjohannes@gmail.com>
2020-01-28 11:43:23 +0100

    cleanup

--- grpc-tester/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/JHeimbach/nfc-cash-system/grpc-tester
-
-go 1.13
-
-require github.com/JHeimbach/nfc-cash-system v0.0.0-20200127130637-45019030f4fb // indirect
--- server/internals/test/db.go
+++ server/internals/test/db.go
@@ -6,9 +6,6 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net"
-	"os"
-	"strings"
-	"testing"
 
 	"github.com/JHeimbach/nfc-cash-system/server/internals/database"
 	"github.com/docker/go-connections/nat"
@@ -22,83 +19,8 @@ const (
 	MysqlDatabase = "mysql_test"
 )
 
-// testDb will hold a db connection for multiple usages
-type testDb struct {
-	db             *sql.DB
-	teardownScript string
-}
-
-// initDb connects to the database. will skip the calling test if TEST_DB_DSN is not set
-// or connection to database is unsuccessful
-func (tdb testDb) initDb(t *testing.T) *sql.DB {
-	if tdb.db != nil {
-		return tdb.db
-	}
-
-	dsn := "${TEST_DB_NAME}:${TEST_DB_NAME}@tcp(${TEST_DB_NAME}-test.db)/${TEST_DB_NAME}?parseTime=true&multiStatements=true"
-	if os.Getenv("TEST_DB_DSN") != "" {
-		dsn = os.Getenv("TEST_DB_DSN")
-	}
-
-	db, err := database.OpenDatabase(dsn)
-	if err != nil {
-		t.Skipf("could not connect to database, skipping test, err: %v", err)
-	}
-	// db.SetConnMaxLifetime(1 * time.Second)
-	// db.SetMaxOpenConns(100)
-	return db
-}
-
-var testDBconn testDb
-
-// getDb connects to database and returns db (*sql.DB) a setup function and a teardown function
-// setup calls migration up scripts to bring database to latest and can
-// receive filenames (strings) for additional setup scripts (sql-files)
-// teardown calls migration down scripts
-//
-// setup and teardown can be called multiple times per test
-func GetDb(t *testing.T, dbName, teardownScriptFile string, migrationDir string) (db *sql.DB, setup func(...string), teardown func()) {
-	t.Helper()
-	db = testDBconn.initDb(t)
-
-	if err := db.Ping(); err != nil {
-		t.Skipf("could not connect to database, skipping test, err: %v", err)
-	}
-
-	testDBconn.teardownScript = func() string {
-		content, err := ioutil.ReadFile(teardownScriptFile)
-		if err != nil {
-			t.Skipf("could not load teardown script: %v", err)
-		}
-		return string(content)
-	}()
-
-	teardown = func() {
-		_, err := db.Exec(testDBconn.teardownScript)
-		if err != nil {
-			t.Fatalf("got error from teardown: %v", err)
-		}
-	}
-
-	setup = func(setupScriptFileNames ...string) {
-		if err := database.UpdateDatabase(db, dbName, migrationDir, true); err != nil {
-			t.Skipf("could not migrate database, err: %v", err)
-		}
-		for _, filename := range setupScriptFileNames {
-			setupScript, _ := ioutil.ReadFile(filename)
-			_, err := db.Exec(string(setupScript))
-			if err != nil {
-				teardown()
-				t.Fatalf("got error initializing script %q: %v", filename, err)
-			}
-		}
-	}
-
-	return db, setup, teardown
-}
-
-func createDSN(server, port string) string {
-	return fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true&multiStatements=true", MysqlUser, MysqlPassword, server, port, MysqlDatabase)
+func createDSN(server string) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&multiStatements=true", MysqlUser, MysqlPassword, server, MysqlDatabase)
 }
 
 func StartDbContainer(networkName string) (local, network string, err error) {
@@ -141,8 +63,6 @@ func StartDbContainer(networkName string) (local, network string, err error) {
 		return "", "", fmt.Errorf("could not start container: %w", err)
 	}
 
-
-
 	mappedPort, err := mysqlC.MappedPort(ctx, mysqlPort)
 	if err != nil {
 		return "", "", fmt.Errorf("could not get internal endpoint address: %w", err)
@@ -164,8 +84,7 @@ func DbConnection() (db *sql.DB, teardown func(), err error) {
 }
 
 func OpenAndMigrateDatabase(addr string) (*sql.DB, error) {
-	a := strings.Split(addr, ":")
-	db, err := database.OpenDatabase(createDSN(a[0], a[1]))
+	db, err := database.OpenDatabase(createDSN(addr))
 	if err != nil {
 		return nil, fmt.Errorf("could not conntect to database: %w", err)
 	}
@@ -198,4 +117,3 @@ func TeardownDB(db *sql.DB, teardownScriptFileName string) func() error {
 		return nil
 	}
 }
-
